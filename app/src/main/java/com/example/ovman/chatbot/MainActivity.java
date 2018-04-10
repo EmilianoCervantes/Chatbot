d56f156d96d6f184e372748012423dbe48fd51e9
EmilianoCervantes <ovman96@hotmail.com>
2018-04-10 12:55:20 -0500

    Chat completo

--- app/src/main/java/com/example/ovman/chatbot/MainActivity.java
+++ app/src/main/java/com/example/ovman/chatbot/MainActivity.java
@@ -1,78 +1,104 @@
 package com.example.ovman.chatbot;
 
-import android.app.Activity;
 import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import ai.api.AIListener;
-import ai.api.android.AIConfiguration;
+import com.google.gson.JsonElement;
+
+import java.util.Map;
+
 import ai.api.AIDataService;
+import ai.api.AIListener;
+import ai.api.AIServiceException;
+
+import ai.api.android.AIConfiguration;
 import ai.api.android.AIService;
 import ai.api.model.AIError;
+import ai.api.model.AIRequest;
 import ai.api.model.AIResponse;
 import ai.api.model.Result;
 
-public class MainActivity extends Activity implements AIListener {
-    private TextView textView;
+
+public class MainActivity extends AppCompatActivity implements AIListener {
     private EditText editText;
     private Button button;
+    private TextView textView;
 
-    //Chatbox
     private AIService aiService;
     private AIDataService aiDataService;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView)findViewById(R.id.chatView);
+        final AIConfiguration config = new AIConfiguration("73e6638ad2ec463388059aefc4fadb68",
+                AIConfiguration.SupportedLanguages.Spanish,
+                AIConfiguration.RecognitionEngine.System);
+
+        aiDataService = new AIDataService(config);
+        aiService = AIService.getService(this, config);
+        aiService.setListener(this);
+
         editText = (EditText)findViewById(R.id.mensaje);
         button = (Button)findViewById(R.id.enviar);
-
-        final AIConfiguration configuration = new AIConfiguration("a3bcd8a5ecc54b3fbb2bd50f4c759366",
-                AIConfiguration.SupportedLanguages.Spanish,
-                AIConfiguration.RecognitionEngine.System
-        );
-
-        aiDataService = new AIDataService(configuration);
-        aiService = AIService.getService(this, configuration);
-        aiService.setListener(this);
+        textView = (TextView)findViewById(R.id.chatView);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //
+                new SendRequestTask(aiDataService).execute(editText.getText().toString());
             }
         });
     }
 
     public class SendRequestTask extends AsyncTask<String, String, AIResponse>{
 
+        private AIDataService aiDataService;
+
+        public SendRequestTask(AIDataService aiDataService) {
+            this.aiDataService = aiDataService;
+        }
+
         @Override
         protected AIResponse doInBackground(String... strings) {
-            return null;
+            AIRequest aiRequest = new AIRequest();
+            AIResponse aiResponse = null;
+            aiRequest.setQuery(strings[0]);
+            try {
+                aiResponse = aiDataService.request(aiRequest);
+            } catch (AIServiceException e) {
+                e.printStackTrace();
+            }
+            return aiResponse;
         }
 
         @Override
         protected void onPostExecute(AIResponse aiResponse) {
             super.onPostExecute(aiResponse);
             Result result = aiResponse.getResult();
-            textView.append("You :"+result.getResolvedQuery()+"\r\n");
-            textView.append("Bot :"+result.getFulfillment().getSpeech()+"\r\n");
+            textView.append("You: " + result.getResolvedQuery() + "\r\n");
+            textView.append("Bot: " + result.getFulfillment().getSpeech() + "\r\n");
         }
     }
 
     @Override
-    public void onResult(AIResponse result) {
-        Result result1 = result.getResult();
+    public void onResult(AIResponse result1) {
+        Result result = result1.getResult();
+        /*
+        String parameterString = "";
+        if(result.getParameters() != null && !result.getParameters().isEmpty()){
+            for(final Map.Entry<String, JsonElement> entry : result.getParameters().entrySet()){
+                parameterString += ""
+            }
 
-        //Obtener parámtetros
-
+        }*/
     }
 
     @Override
